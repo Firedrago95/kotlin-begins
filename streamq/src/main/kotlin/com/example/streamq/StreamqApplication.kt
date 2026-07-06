0216634dfad7ed4a42afd5ed074ac83ec90bfd34
Firedrago95 <yonghwa95@naver.com>
2026-07-06 21:05:41 +0900

    feat: 엔티티 생성

--- streamq/src/main/kotlin/com/example/streamq/StreamqApplication.kt
+++ streamq/src/main/kotlin/com/example/streamq/StreamqApplication.kt
@@ -1,8 +1,11 @@
 package com.example.streamq
 
+import jakarta.persistence.EntityListeners
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class StreamqApplication
 
---
+++ streamq/src/main/kotlin/com/example/streamq/domain/user/User.kt
@@ -0,0 +1,40 @@
+package com.example.streamq.domain.user
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+
+@Entity
+@Table(name="users")
+@EntityListeners(AuditingEntityListener::class)
+class User (
+    @Column(unique = true, nullable = false)
+    val email: String,
+
+    @Column(nullable = false)
+    var passwordHash: String,
+
+    @Column(nullable = false)
+    var name: String,
+
+    @Enumerated(EnumType.STRING)
+    var role: Role = Role.MEMBER,
+){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: ZonedDateTime? = null
+}
