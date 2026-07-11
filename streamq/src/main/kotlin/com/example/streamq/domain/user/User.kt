3d6a52a3ec9ee3e9b83eb4cb9e17457571a047a1
Firedrago95 <yonghwa95@naver.com>
2026-07-11 20:09:13 +0900

    fix: User관련 로직 롤백

--- streamq/src/main/kotlin/com/example/streamq/domain/user/User.kt
+++ streamq/src/main/kotlin/com/example/streamq/domain/user/User.kt
@@ -9,32 +9,48 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 @Entity
-@Table(name = "users")
+@Table(
+    name="users",
+    uniqueConstraints = [
+        UniqueConstraint(name = "uk_provider_provider_id", columnNames = ["provider", "provider_id"])
+    ]
+)
 @EntityListeners(AuditingEntityListener::class)
-class User(
-    @Column(nullable = false, unique = true)
+class User (
+    @Column(nullable = false)
     val email: String,
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    var passwordHash: String,
+    val provider: Provider,
+
+    @Column(nullable = false)
+    val providerId: String,
 
     @Column(nullable = false)
     var name: String,
 
+    var picture: String? = null,
+
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    var role: Role = Role.MEMBER
-) {
+    var role: Role = Role.MEMBER,
+){
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
+    val id: Long = 0L
 
     @CreatedDate
-    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    @Column(updatable = false)
     var createdAt: ZonedDateTime? = null
+
+    @LastModifiedDate
+    var updatedAt: ZonedDateTime? = null
 }
